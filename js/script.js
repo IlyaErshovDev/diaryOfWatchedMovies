a349e37c4f8f98cbe09f3b98fb0848a27a240c47
IlyaErshovDev <yosh7595@gmail.com>
2020-08-27 17:22:54 +0300

    Добавлена обработка ответов пользователя и прочий функционал

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
 'use strict';
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+if (numberOfFilms < 0) {
+  console.log("Ошибка ввода");
+  const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+} else if (numberOfFilms == 0) {
+  console.log("Мир кинематографа велик! Скорее познакомьтесь с ним и возвращайтесь! =)");
+} else {
+  const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+  };
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
+  for (let i = 0; i < 2; i++) {
+    const film = prompt('Один из последних просмотренных фильмов?', ''),
+      score = prompt('На сколько вы оцените его?', '');
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-  b = prompt('На сколько вы оцените его?', ''),
-  c = prompt('Один из последних просмотренных фильмов?', ''),
-  d = prompt('На сколько вы оцените его?', '');
+    if (film != null && score != null && film != '' && score != '' && film.length < 50) {
+      personalMovieDB.movies[film] = score;
+    } else {
+      console.log('Ошибка, повторите ввод');
+      i--;
+    }
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log('personalMovieDB: ', personalMovieDB);
+
+  }
+
+  console.log('personalMovieDB: ', personalMovieDB);
+  if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+  } else if (10 >= personalMovieDB.count && personalMovieDB.count <= 30) {
+    console.log('Вы - классический зритель');
+  } else if (personalMovieDB.count > 30) {
+    console.log('Вы - киноман!');
+  } else {
+    console.log("Произошла ошибка");
+  }
+}
